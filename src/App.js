59e7121ced31a1e312c4de0f1f16b0575f84aebe
Jana <jana.samson@hotmail.fr>
2022-06-30 20:24:52 -0400

    searchy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./components/Home";
 
 function App() {
+  const [userCityInput, setUserCityInput] = useState(null);
+  const [cityName, setCityName] = useState(null);
+  const [cityCoord, setCityCoord] = useState(null);
+  const [data, setData] = useState(null);
+  // let resultsElem = "";
+
+  function handleUserInput(e) {
+    setUserCityInput(e.target.value);
+  }
+
+  function handleCityName() {
+    setCityName(userCityInput);
+  }
+
+  useEffect(() => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=30df8105cc64445635b2c2d7452a1466`
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [cityName]);
+
+  let resultsElem = (
+    <div>
+      {data?.map(({ name, country, state }) => (
+        <h2>
+          {name}, {state}, {country}
+        </h2>
+      ))}
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home handleUserInput={handleUserInput} handleCityName={handleCityName} />
+      <div>{resultsElem}</div>
     </div>
   );
 }
